14e2a07402dac28a4016b92d97ce48c8e6227d06
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-08 09:49:48 +0300

    last two deliverables pending

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner = "Chris"):
+        self.name = name
+        self.owner = owner
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception
+        self.pet_type = pet_type
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception
+        pet.owner = self
+    
+    # def get_sorted_pets(self):
+    #     return [pet for pet in Pet.all if pet.owner == self]
+
+
+
+
