d958b8c1ac73ee0b534ef07060a27d760529016c
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-21 12:15:38 +0300

    one to many lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -3,32 +3,31 @@ class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
     all = []
 
-    def __init__(self, name, pet_type, owner = None):
+    def __init__(self, name, pet_type, owner = "Chris"):
         if pet_type not in Pet.PET_TYPES:
             raise Exception
         self.name = name
         self.pet_type = pet_type
-        self.owner = owner 
-        if owner:
-            owner.add_pet(self)
+        self.owner = owner
         Pet.all.append(self)
 
 class Owner:
     def __init__(self, name):
-        self.name = name
+        self.name = name 
         self._pets = []
     
     def pets(self):
-        return self._pets
+        return [pet for pet in Pet.all if pet.owner == self]
     
     def add_pet(self, pet):
         if not isinstance(pet, Pet):
             raise Exception
         pet.owner = self
         self._pets.append(pet)
-            
+    
     def get_sorted_pets(self):
-        return sorted(self._pets, key=lambda pet: pet.name)
+        return sorted([pet for pet in Pet.all if pet.owner == self], key = lambda pet: pet.name)
+
 
 
 
